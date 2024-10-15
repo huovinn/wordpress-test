5dfe312418b43d9aa1697e10ff01435c908a3f4a
Janne Huovinen <janne.huovinen@ambientia.fi>
2024-10-15 12:38:21 +0300

    os

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', getenv('WORDPRESS_DB_USER'));
 define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', getenv('WORDPRESS_DB_HOST'));
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
