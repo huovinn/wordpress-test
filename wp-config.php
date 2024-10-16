83ab45329977c7c5e2b27735d3897a90b01fccb3
Janne Huovinen <janne.huovinen@ambientia.fi>
2024-10-16 13:00:15 +0300

    mysql connection

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,10 @@ define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define( 'MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL );
+
+define('MYSQL_SSL_CERT', getenv('AZURE_SQL_SSL_CA_PATH'));
+
 /**#@+
  * Authentication unique keys and salts.
  *
