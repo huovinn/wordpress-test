03e661fbb5ab379bb39d2bdd2dcd4c7163af26b2
Janne Huovinen <janne.huovinen@ambientia.fi>
2024-10-16 14:05:40 +0300

    siteurl

--- wp-config.php
+++ wp-config.php
@@ -18,6 +18,9 @@
  * @package WordPress
  */
 
+define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);
+define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST']);
+
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', getenv('WORDPRESS_DB_NAME'));
