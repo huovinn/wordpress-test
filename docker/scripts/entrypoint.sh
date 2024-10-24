1142237da09564ee0de733d38688b7c40025af96
Janne Huovinen <janne.huovinen@ambientia.fi>
2024-10-24 14:51:55 +0300

    copy contents from mounted dir

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Copy wp-content to a temporary location before the volume is mounted
-mkdir /tmp/wp-content-backup
-cp -R /opt/app-root/src/wp-content /tmp/wp-content-backup
-
 # Check if the volume-mounted wp-content directory is empty
 if [ -z "$(ls -A /opt/app-root/src/wp-content)" ]; then
-    echo "wp-content directory in the volume is empty. Copying backup from image..."
-    # Copy the backup from /tmp/wp-content-backup to the volume
-    cp -R /tmp/wp-content-backup/* /opt/app-root/src/wp-content/
+    echo "wp-content directory in the volume is empty. Copying content from the directory that the init container created"
+  
+    cp -R /wp-content-backup/* /opt/app-root/src/wp-content/
 else
     echo "wp-content directory in the volume is already populated."
 fi
